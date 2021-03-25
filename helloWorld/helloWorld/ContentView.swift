6e61839d5b9a83817ca3d78c2668fe020ac12fb5
ios.factory.dev <ios.factory.dev@gmail.com>
2021-03-25 10:27:36 +0300

    2021-03-25_10-25

---
+++ helloWorld/helloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloWorld
+//
+//  Created by Admin on 25.03.2021.
+//  Copyright © 2021 iosfactorydev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World, BIG BOSS!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
