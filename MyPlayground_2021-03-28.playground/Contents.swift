bb9f0b76803e96738fd6574d81b2e33a8ebc9387
ios.factory.dev <ios.factory.dev@gmail.com>
2021-03-28 19:29:52 +0300

    2021_03_28_19-30

---
+++ MyPlayground_2021-03-28.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//var str = "Hello, playground"
+//let str1 = "hello, loh"
+//
+//str = "tupitsa"
+//str1 = "totot"
+
+let x: Character = "q"
+let y: String = "Belarus"
+let z: Double = 1.000000000005
+let zz: Float = 1.0000077
+let d: Int = Int(123.123)
+let b: Bool = false
+
+let rr = false
+
+let rrr = Int(123.12345)
+
+let wer = Float(123.1231231231232424)
