ba2a1249ee4f5065b5d4bcd309bb53a878f9db1d
ios.factory.dev <ios.factory.dev@gmail.com>
2021-03-29 09:28:22 +0300

    2021-03-29_09-28

---
+++ MyPlayground_2021-03-29.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let c: Float = 10/3
+
+let d = 100 % 3
+
+let e = 3
+var i = 1
+
+//i = i + e
+
+i += e
+
+let defName = "Fed"
+
+var name: String? = "sdfsfd"
+
+let myName = name ?? defName
+
+let bl = false
+
+let t = bl ? 10 : 30
+
+let temp = 21
+let wind = 3
+
+if temp >= 20 && wind <= 5 {
+    print ("go gulyat")
+}
+
+let bl1 = true
+let bl2 = !bl1
+
+
+
+}
+
+
