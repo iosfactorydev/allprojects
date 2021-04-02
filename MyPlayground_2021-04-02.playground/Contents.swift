288b3403aef3e43c6e510eddfe76e59ce5f4b674
ios.factory.dev <ios.factory.dev@gmail.com>
2021-04-02 13:44:27 +0300

    2021-04-02

---
+++ MyPlayground_2021-04-02.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let temp = 12
+let wind = 7
+
+let weatherDescr = "temp 10 gradusov, wind 20 metrov v secundu"
+
+let weatherDescrI = "temp \(temp) gradusov, wind \(wind) metrov v secundu"
+
+let stringgg = "\(1+4)" + "\(3+7)"
+
+let stringgg1 = "\(1+4) + \(3+7)"
+
+let i = "4"
+let j = i + i
+
+import Foundation
+let number: Float = 9
+print("Число \(number) в квадрате \(number * number), и его квадратный корень \(sqrt(number))")
+
+let x: Int = 10
+x += 10
+print("We have got \(x) cakes")
